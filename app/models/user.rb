f809882180dca3f04b51498a8eb26ed4a18b8c35
Ryan Kelley <rskelley9@gmail.com>
2014-03-23 07:36:06 -0400

    adding user post functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,11 @@ class User < ActiveRecord::Base
     email
   end
 
+  # It also uses an optional forem_email method for displaying avatars using Gravatar. It defaults to email. If you don't have an email attribute on the model, define a new method:
+
+  # def forem_email
+  #   email_address
+  # end
+
 
 end
