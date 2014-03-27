bf0632308ac9286e98317a8c187e0413f152aee3
Ryan Kelley <rskelley9@gmail.com>
2014-03-27 08:41:21 -0400

    WIP: adding TinyMCE rte

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
+  #fields user can update with devise registration#edit
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:account_update) { |u|
       u.permit(:password, :username, :password_confirmation, :current_password)
